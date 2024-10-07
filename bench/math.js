1f0d52b6579258991212512376453c699be0c4dc
Ron Bravo <ronbravo1701@gmail.com>
2024-10-06 20:54:41 -0700

    - Setting up the project.

---
+++ bench/math.js
@@ -0,0 +1,9 @@
+export const sum = (a, b) => a + b;
+export const div = (a, b) => a / b;
+export const mod = (a, b) => a % b;
+
+export default {
+  sum,
+  div,
+  mod,
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+
+function describe () {}
+
+function it () {}
